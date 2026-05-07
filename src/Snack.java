67d46e43b98ad0c506f39502322691eefe0dd1bc
Hardhik.B08 <105904475+Agenthardhik@users.noreply.github.com>
2026-05-07 13:57:21 +0530

    Update Snack.java

--- src/Snack.java
+++ src/Snack.java
@@ -1,17 +1,64 @@
+package oops;
+
 class Snack {
     private String name;
     private double price;
 
+    
     public Snack(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
+    
     public String getName() {
         return name;
     }
 
     public double getPrice() {
         return price;
+    }
+}
+
+class SnackOrder {
+    private Snack snack1, snack2;
+    private int qty1, qty2;
+
+   
+    public void addFirstSnack(Snack s, int q) {
+        snack1 = s;
+        qty1 = q;
+    }
+
+    
+    public void addSecondSnack(Snack s, int q) {
+        snack2 = s;
+        qty2 = q;
+    }
+
+    
+    public double calculateSnackTotal() {
+        double total = 0;
+
+        if (snack1 != null) {
+            total += snack1.getPrice() * qty1;
+        }
+
+        if (snack2 != null) {
+            total += snack2.getPrice() * qty2;
+        }
+
+        return total;
+    }
+
+    
+    public void showSnacks() {
+        if (snack1 != null) {
+            System.out.println(snack1.getName() + " x " + qty1);
+        }
+
+        if (snack2 != null) {
+            System.out.println(snack2.getName() + " x " + qty2);
+        }
     }
 }
